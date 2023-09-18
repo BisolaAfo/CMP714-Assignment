be337c19d18fa71332182efbaf45859c465fe505
Abisola Olufioye <bisolaolufioye@gmail.com>
2023-09-18 12:12:43 +0100

    first commit

--- src/Evennumbers.java
+++ src/Evennumbers.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Evennumbers {
+public class Evennumbers {
+
+    public static void main(String[]args) {
+        int number = {6,7,8,2,9,13};
+        for(int  i=1; 1<=number; i++);
+        System.out.println(i);
+    }
 }
